bc62b9dcfbb060ed15885fb7477444e10c64ab55
EtiTheSpirit <subzeroxan@gmail.com>
2022-08-05 06:55:08 -0500

    Add project files.

---
+++ DepthMode/DepthMode.cs
@@ -0,0 +1,34 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DepthModeMod {
+	public class DepthMode : MelonMod {
+
+		/// <summary>
+		/// The frequency at which <see cref="Camera.main"/> is referenced and has its depth mode updated.
+		/// </summary>
+		private const float UPDATE_RATE = 0.5f;
+
+		/// <summary>
+		/// The amount of time counted since the last OnUpdate call.
+		/// </summary>
+		private float _totalDelta = 0;
+
+		public override void OnApplicationStart() {
+			Prefs.InitializePrefs();
+		}
+
+		public override void OnUpdate() {
+			_totalDelta += Time.deltaTime;
+			if (_totalDelta >= UPDATE_RATE) {
+				_totalDelta = 0;
+				Camera.main.depthTextureMode = Prefs.DepthMode;
+			}
+		}
+	}
+}
---
+++ DepthMode/Prefs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace DepthModeMod {
+	public class Prefs {
+
+		private static MelonPreferences_Category _settingsCategory { get; set; }
+
+		/// <summary>
+		/// The current user-defined depth texture mode based on their preferences.
+		/// This property is computed every time it is referenced.
+		/// </summary>
+		public static DepthTextureMode DepthMode {
+			get {
+				DepthTextureMode mode = DepthTextureMode.None;
+				if (_settingsCategory == null) return mode;
+				if (_settingsCategory.GetEntry<bool>("ExposeDepth").Value) mode |= DepthTextureMode.Depth;
+				if (_settingsCategory.GetEntry<bool>("ExposeNormals").Value) mode |= DepthTextureMode.DepthNormals;
+				if (_settingsCategory.GetEntry<bool>("ExposeMotion").Value) mode |= DepthTextureMode.MotionVectors;
+				return mode;
+			}
+		}
+
+		/// <summary>
+		/// Called by the mod to create the default preferences.
+		/// </summary>
+		public static void InitializePrefs() {
+			_settingsCategory = MelonPreferences.CreateCategory("Depth Mode Setter");
+			_settingsCategory.CreateEntry("ExposeDepth", true, "Expose Depth", "If true, the Depth Texture is enabled.");
+			_settingsCategory.CreateEntry("ExposeNormals", false, "Expose Depth Normals", "If true, the Depth Normals Texture is enabled.");
+			_settingsCategory.CreateEntry("ExposeMotion", false, "Expose Motion Vectors", "If true, the Motion Vectors Texture is enabled.");
+			_settingsCategory.SaveToFile();
+		}
+
+	}
+}
