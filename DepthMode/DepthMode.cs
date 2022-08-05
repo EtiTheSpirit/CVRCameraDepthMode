2a89962e0972c8f44a59a4ae061fb65be915fefc
EtiTheSpirit <subzeroxan@gmail.com>
2022-08-05 07:22:12 -0500

    Optimize by caching the main camera

--- DepthMode/DepthMode.cs
+++ DepthMode/DepthMode.cs
@@ -20,19 +20,31 @@ namespace DepthModeMod {
 		/// </summary>
 		private float _totalDelta = 0;
 
+		private Camera _lastKnownMainCamera = null;
+
 		public override void OnApplicationStart() {
 			Prefs.InitializePrefs();
 		}
 
 		public override void OnUpdate() {
-			// TODO: Camera.current? The VRC mod I made for my Seeker used Camera.main so I will use that here too.
-			Camera mainCamera = Camera.main;
-			if (mainCamera == null) return;
-
 			_totalDelta += Time.deltaTime;
 			if (_totalDelta >= UPDATE_DELAY) {
 				_totalDelta = 0;
-				mainCamera.depthTextureMode = Prefs.DepthMode;
+
+				// TODO: Camera.current? The VRC mod I made for my avatar used Camera.main so I will use that here too.
+				// NOTE: I place this code here (in the if statement) because according to unity, indexing Camera.main is "not cheap"
+				// Quote: "Accessing this property has a small CPU overhead, comparable to calling GameObject.GetComponent.
+				//			Where CPU performance is important, consider caching this property."
+				if (_lastKnownMainCamera == null || _lastKnownMainCamera.gameObject == null) {
+					// afaik Unity will return true on ==null if the Camera has been destroyed
+					// but just to be safe I want to check gameObject too. I do not know if this is necessary.
+
+					Camera mainCamera = Camera.main;
+					if (mainCamera == null) return;
+					_lastKnownMainCamera = mainCamera;
+				}
+
+				_lastKnownMainCamera.depthTextureMode = Prefs.DepthMode;
 			}
 		}
 	}
